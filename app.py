85ab6e2caf856e8db9f44065cf98c062b7e37e7b
Rody <hellorody1234+1@gmail.com>
2025-09-24 16:35:10 +0900

    complete todo app

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return 'test update'
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+
+
+### タスクを表示する ###
+
+### タスク追加 ###
+
+### タスク削除 ###
+
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
