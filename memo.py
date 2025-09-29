077aa2add23a619b70fe7010c1860f9974f6c13a
Rody <hellorody1234+1@gmail.com>
2025-09-30 00:46:27 +0900

    update memo.py

--- memo.py
+++ memo.py
@@ -3,18 +3,31 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 memos = []
 id = 0
+
 @app.route('/')
 def index():
-    return render_template('memo.html')
+    return render_template('memo.html',memos=memos)
 
 @app.route('/add',methods=['post'])
 def add():
+    global id
     title = request.form['title']
     text = request.form['text']
     tmp = {'id':id,'title':title,'text':text}
     memos.append(tmp)
+    id += 1
     print(memos)
     return render_template('memo.html',memos=memos)
+
+@app.route('/delete', methods=['post'])
+def delete():
+    delete_id = request.form['id']
+    for i in range(len(memos)):
+        if str(memos[i]['id']) == delete_id:
+            memos.pop(i)
+            break
+    return render_template('memo.html',memos=memos)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
