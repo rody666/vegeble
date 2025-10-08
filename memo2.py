ce11f321bb7576d1165382a350ea25775d5a87ab
Rody <hellorody1234+1@gmail.com>
2025-10-09 02:51:37 +0900

    learning class

--- memo2.py
+++ memo2.py
@@ -8,3 +8,27 @@ class Car:
         self.speed -= 10
     def __repr__(self):
         return f'<Car brand={self.brand} speed={self.speed}>'
+
+cars = []
+car_specs = {
+    'toyota':100,
+    'mazda':120,
+    'audi':40,
+    'ferrari':300,
+    'hyundai':110
+}
+
+for car_brand in car_specs:
+    car_object = Car(car_brand,car_specs[car_brand])
+    cars.append(car_object)
+
+print('before accelerating:')
+for car in cars:
+    print(car)
+
+for car in cars:
+    car.accelerate()
+
+print('after accelerating:')
+for car in cars:
+    print(car)
