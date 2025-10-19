8dd227a9c620312e7b512b0b5ba011ead6dd2853
Rody <hellorody1234+1@gmail.com>
2025-10-20 00:51:21 +0900

    make app

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('assenssment.html')
+
+@app.route('/assenssment')
+def assenssment():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
